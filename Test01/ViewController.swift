63ea76650437d19ec655f67ebd8d78fae58e2ea8
Jay Bergonia <jay.b@tektosdesign.com>
2018-05-29 14:52:09 +0800

    Initial Commit

---
+++ Test01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test01
+//
+//  Created by Jay Bergonia on 29/5/2018.
+//  Copyright © 2018 Tektos Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
